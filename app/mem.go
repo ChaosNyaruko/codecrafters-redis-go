986a8f5e025e272f609a9eec04557124cc226888
ChaosNyaruko <cabbageparadise@gmail.com>
2026-06-28 18:03:15 +0800

    codecrafters submit [skip ci]

--- app/mem.go
+++ app/mem.go
@@ -32,7 +32,7 @@ type Store struct {
 func NewStore() *Store {
 	return &Store{
 		store:           map[string]item{},
-		t:               time.NewTicker(1 * time.Millisecond),
+		t:               time.NewTicker(50 * time.Microsecond),
 		blockingClients: map[string][]*clientStatus{},
 	}
 }
